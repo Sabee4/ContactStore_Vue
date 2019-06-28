59310409cdf9d07f321407f782e8b10409894a3d
sabeena <sabeenap@gmail.com>
2019-06-28 06:17:28 +0100

    CRUD on Contacts

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>ContactStore</title>
+    <link rel="stylesheet" href="/contactstore/public/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      <div id="app">
+        <contacts></contacts>
+      </div>
+    </div>
+
+    <script src="/contactstore/public/js/app.js"></script>
+  </body>
+</html>
